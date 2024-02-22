1f290554569b507682e18480d6c4c39a028ab569
Lavina-23 <lavinakohai3227@gmail.com>
2024-02-22 14:38:45 +0700

    feat[ASD]: object practicum

---
+++ SEMESTER2/Algoritma Struktur Data/praktikum/P2 Object/code/Buku14.java
@@ -0,0 +1,41 @@
+public class Buku14 {
+
+    String judul, pengarang;
+    int halaman, stok, harga;
+
+    public Buku14() {
+
+    }
+
+    public Buku14(String jud, String pg, int hal, int stok, int har) {
+        judul = jud;
+        pengarang = pg;
+        halaman = hal;
+        this.stok = stok;
+        harga = har;
+    }
+
+    void tampilInformasi() {
+        System.out.println("Judul: " + judul);
+        System.out.println("Pengarang: " + pengarang);
+        System.out.println("Jumlah halaman: " + halaman);
+        System.out.println("Sisa stok: " + stok);
+        System.out.println("Harga: Rp " + harga);
+    }
+
+    void terjual(int jml) {
+        if (stok > 0) {
+            stok -= jml;
+        } else {
+            System.out.println("Stok Habis");
+        }
+    }
+
+    void restock(int jml) {
+        stok += jml;
+    }
+
+    void gantiHarga(int hrg) {
+        harga = hrg;
+    }
+}
